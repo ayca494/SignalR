dd1aa19ac8a3d838394ebe41c8b71d014a515492
ayca494 <aycaturkmen46@gmail.com>
2023-01-03 17:54:04 +0300

    Add project files.

---
+++ SignalR_API/Hubs/MyHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_API.Hubs
+{
+    public class MyHub:Hub
+    {
+        //Clientların çağıracağı metodları tanımlıyoruz asenkron olarak 
+
+        public static List<string> Names { get; set; } = new List<string>(); //static olarak tanımlamamızın sebebi MyHub classını her çağırıldığında bir nesne oluşturulması 
+        //uygulamamız çalıştığı sürece Names verilerine erişebileceğiz static yaptığımız için gerçek hayatta proje yaparsak veritabanına kayıt edebiliriz şimdi memoryde geçici tutacağız.
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            //Clientlardaki metodların çalışması için bir istek göndereceğiz 
+            await Clients.All.SendAsync("ReceiveName", name); //bağlı olan tüm clientlara istek gönderir "ReceiveMessage" Clientlardaki metodun adıdır. yanına da data gönderebiliriz message gibi
+            
+        }
+
+        //Clientda o anda memoryde olan tüm stringleri alan metod
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+    }
+}
---
+++ SignalR_API/Program.cs
@@ -0,0 +1,43 @@
+using SignalR_API.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("https://localhost:44353").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+    });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//localhost:4400/MyHub þeklinde girerlerse URL ulaþsýnlar diye belirtiyoruz
+app.MapHub<MyHub>("/MapHub");
+
+app.Run();
